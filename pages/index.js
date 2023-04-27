96e39885f6751dc5bd43edea1ca4618fce202045
samboleap <samboleap3008@gmail.com>
2023-04-27 10:07:15 +0700

    funny repo

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,6 +16,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
+        <h1 className={styles.title}>
+          <Link href="/leap/demo">Go to New Page</Link>
+        </h1>
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
---
+++ pages/leap/demo.js
@@ -0,0 +1,62 @@
+import Image from "next/image";
+import Marquee from "react-fast-marquee";
+import snatoch from "../assetImage/snatoch.jpg"
+import bekkbl from "../assetImage/bekkbal.jpg"
+import leng from "../assetImage/leng.jpg"
+import naro from "../assetImage/naro.jpg"
+import snareturn from "../assetImage/snareturn.jpg"
+
+export default function demo(){
+    return (
+        <>
+ <h1 style={Syt}>Collection Image</h1>
+ <Marquee speed={160} direction="left" delay={0.4} height={100}>
+   <Image
+                src={bekkbl}
+                alt="Vercel Logo"
+                width={550}
+                height={800}
+                priority
+              />
+   <Image
+                src={leng}
+                alt="Vercel Logo"
+                width={550}
+                height={800}
+                priority
+              />
+   <Image
+                src={naro}
+                alt="Vercel Logo"
+                width={550}
+                height={800}
+                priority
+              />
+   <Image
+                src={snatoch}
+                alt="Vercel Logo"
+                width={550}
+                height={800}
+                priority
+              />
+   <Image
+                src={snareturn}
+                alt="Vercel Logo"
+                width={550}
+                height={800}
+                priority
+              />
+   
+ </Marquee>
+ </>
+ )
+}
+const Syt={
+   color: 'blue',
+   fontSize: '40px',
+   fontWeight: 'bold',
+   fontFamily: 'Arial',
+   textAlign: 'center'
+}
+
+  
